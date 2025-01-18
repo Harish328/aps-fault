e97cff7ac2a24fcedc6c1d0efe38acefb112d8bf
hsdfhjkh d@ksdfghk .fdshsdk <dsadsfgjhgdsf@g>
2025-01-18 07:01:07 +0530

    dump data into mongodb

---
+++ data_dump.py
@@ -0,0 +1,32 @@
+import pymongo
+import pandas as pd
+import json
+# Provide the mongodb localhost url to connect python to mongodb.
+client = pymongo.MongoClient("mongodb+srv://harish:HarishK96@cluster0.hwi83x3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+DATA_FILE_PATH="aps_failure_training_set1.csv"
+DATABASE_NAME="aps"
+COLLECTION_NAME="sensor"
+
+
+if __name__=="__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns: {df.shape}")
+
+    
+#Convert dataframe to json so that we can dump these record in mongo db
+    df.reset_index(drop=True,inplace=True)
+
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+
+    #insert converted json record to mongo db
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+
+
+
+
+
+
+
+
